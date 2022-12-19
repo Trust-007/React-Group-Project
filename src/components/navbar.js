0db93ab1be51da19e51a370015294d79431bf1d2
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-12-19 15:44:11 +0100

    create the navbar with the logo

---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../images/logo.png';
+
+const Navbar = () => (
+  <header>
+    <div>
+      <img width="50" height="50" alt="logo" src={logo} />
+    </div>
+  </header>
+);
+
+export default Navbar;
