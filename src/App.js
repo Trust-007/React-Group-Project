a1e65013523247d987f645274796f5bad51643cf
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-12-20 09:54:09 +0100

    dsplay data from the redux store

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import Home from './pages/Home';
-import Mission from './pages/Mission';
 import Profile from './pages/Profile';
 import fetchRockets from './redux/rockets/rocketThunk';
+import Missionpage from './pages/Mission';
 
 function App() {
   const dispatch = useDispatch();
@@ -15,7 +15,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Navigate to="/rockets" />} replace />
       <Route path="/rockets" element={<Home />} />
-      <Route path="/missions" element={<Mission />} />
+      <Route path="/missions" element={<Missionpage />} />
       <Route path="/profile" element={<Profile />} />
     </Routes>
   );
