55f53d198a64228669d38d4e83b770cc6a979d46
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-12-21 20:01:04 +0100

    fix linter errors

--- src/_test_/mission.react.test.js
+++ src/_test_/mission.react.test.js
@@ -2,6 +2,7 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import MissionList from '../components/missions/missionList';
 import store from '../redux/configureStore';
+import Missionpage from '../pages/Mission';
 import { fetchMission, joinMission, leavingMission } from '../redux/missions/missionReducer';
 
 describe('display the mission in the page', () => {
